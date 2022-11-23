1d573a89e358c7194957c3e709d51d4f6c8719fd
沈军 <shenjun@qccr.com>
2022-11-23 20:06:33 +0800

    feat [ complete Lab1 and passed! ]

--- src/java/simpledb/storage/Tuple.java
+++ src/java/simpledb/storage/Tuple.java
@@ -14,13 +14,13 @@ public class Tuple implements Serializable {
 
     private TupleDesc tupleDesc;
 
-    private  RecordId recordId;
+    private RecordId recordId;
+
     /**
      * Create a new tuple with the specified schema (type).
      *
-     * @param td
-     *            the schema of this tuple. It must be a valid TupleDesc
-     *            instance with at least one field.
+     * @param td the schema of this tuple. It must be a valid TupleDesc
+     *           instance with at least one field.
      */
     public Tuple(TupleDesc td) {
         this.tupleDesc = td;
@@ -35,7 +35,7 @@ public class Tuple implements Serializable {
 
     /**
      * @return The RecordId representing the location of this tuple on disk. May
-     *         be null.
+     * be null.
      */
     public RecordId getRecordId() {
         return this.recordId;
@@ -44,69 +44,63 @@ public class Tuple implements Serializable {
     /**
      * Set the RecordId information for this tuple.
      *
-     * @param rid
-     *            the new RecordId for this tuple.
+     * @param rid the new RecordId for this tuple.
      */
     public void setRecordId(RecordId rid) {
         this.recordId = rid;
     }
 
 
-//    private Map<Integer,Field> map = new HashMap<>();
-    private List<Field> list = new ArrayList<>();
+    //    private Map<Integer,Field> map = new HashMap<>();
+    private List<Field> fields = new ArrayList<>();
+
     /**
      * Change the value of the ith field of this tuple.
      *
-     * @param i
-     *            index of the field to change. It must be a valid index.
-     * @param f
-     *            new value for the field.
+     * @param i index of the field to change. It must be a valid index.
+     * @param f new value for the field.
      */
     public void setField(int i, Field f) {
-        if(tupleDesc.getFieldType(i) != f.getType()){
-            throw  new UnsupportedOperationException("file type is "+ tupleDesc.getFieldType(i).name());
+        if (tupleDesc.getFieldType(i) != f.getType()) {
+            throw new UnsupportedOperationException("file type is " + tupleDesc.getFieldType(i).name());
         }
-        list.add(i,f);
+        fields.add(i, f);
     }
 
     /**
+     * @param i field index to return. Must be a valid index.
      * @return the value of the ith field, or null if it has not been set.
-     *
-     * @param i
-     *            field index to return. Must be a valid index.
      */
     public Field getField(int i) {
-        return list.get(i);
+        if (i >= this.fields.size() || i < 0) {
+            throw new ArrayIndexOutOfBoundsException(i);
+        }
+        return fields.get(i);
     }
 
     /**
      * Returns the contents of this Tuple as a string. Note that to pass the
      * system tests, the format needs to be as follows:
-     *
+     * <p>
      * column1\tcolumn2\tcolumn3\t...\tcolumnN
-     *
+     * <p>
      * where \t is any whitespace (except a newline)
      */
     public String toString() {
-        // some code goes here
-        throw new UnsupportedOperationException("Implement this");
+        return super.toString();
     }
 
     /**
-     * @return
-     *        An iterator which iterates over all the fields of this tuple
-     * */
-    public Iterator<Field> fields()
-    {
-        // some code goes here
-        return null;
+     * @return An iterator which iterates over all the fields of this tuple
+     */
+    public Iterator<Field> fields() {
+        return this.fields.iterator();
     }
 
     /**
      * reset the TupleDesc of this tuple (only affecting the TupleDesc)
-     * */
-    public void resetTupleDesc(TupleDesc td)
-    {
+     */
+    public void resetTupleDesc(TupleDesc td) {
         // some code goes here
     }
 }
